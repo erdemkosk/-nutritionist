ee716d5449e288f5aa8a5d0b138fb06a3b9f32d3
Erdem Kosk <erdem.kosk@getir.com>
2022-12-22 16:31:50 +0300

    Changed image url

--- components/HeroIllustation.tsx
+++ components/HeroIllustation.tsx
@@ -2,6 +2,6 @@ import NextImage from 'next/image';
 
 export default function HeroIllustration() {
   return (
-    <NextImage src="/../public/Healthy-lifestyle.svg" height={600} width={500} />
+    <NextImage src="/Healthy-lifestyle.svg" height={600} width={500} />
   )
 }
